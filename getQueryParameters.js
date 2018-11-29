9ce869712c323bfab86d505b7ae22cd7501e5a51
adrianndwiga <adrian_ndwiga@hotmail.com>
2018-11-29 20:48:53 +0000

    added functionality to load from aws s3

---
+++ getQueryParameters.js
@@ -0,0 +1,20 @@
+function getQueryParameters() {
+    let props = {
+        start: '', 
+        end: '',
+        bucket: '', 
+        key: '',
+        includes: []
+    }
+    process.argv.forEach(param => {
+        const parts = param.split(':');
+        if(parts.length > 0 && props[parts[0].replace('-', '')] === '') {
+            props[parts[0].replace('-', '')] = parts[1];
+        } else if(parts.length > 0 && props[parts[0].replace('-', '')] && props[parts[0].replace('-', '')].length === 0) {
+            props[parts[0].replace('-', '')] = parts[1].split(',');
+        }
+    });
+    return props;
+}
+
+module.exports = getQueryParameters;
---
+++ readme.md
@@ -0,0 +1,1 @@
+node ./load-s3.js -start:1/1/2018 -end:31/10/2018 -bucket:bucket -key:filename -includes:query
